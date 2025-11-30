b58936bb7b47f98e38296e74d9f0c27a3243ebf8
Maria Gajek <gajekmaria14@gmail.com>
2025-11-30 19:43:42 +0100

    extentended setup with link

--- setup_project.sh
+++ setup_project.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-set -ex
+set -x
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 
 cd build
 cmake ..
 cmake --build .
+
+cd ..
+
+ln -s $(pwd)/mors_lib $(python3.13 -m site --user-site)/mors_lib || true
+for f in ~/.profile ~/.bashrc ~/.zshrc; do
+    [ -f "$f" ] && echo "export PATH=\"$SCRIPT_DIR/build:\$PATH\"" >> "$f"
+    source $f
+done
